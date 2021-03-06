1ad6da6e270acaa85685a083b83abf1aa977ddf5
elKuston <romolocaponera@uma.es>
2021-03-06 19:27:44 +0100

    Incremento de las variables incluida en  la politica

--- politica.py
+++ politica.py
@@ -6,8 +6,28 @@ import sys
 
 
 class Politica(ABC):
+    def __init__(self, parametro, variacion_parametro, semilla_random=0):
+        self.parametro = parametro
+        self.semilla = semilla_random
+        self.variacion_parametro = variacion_parametro
+        self.variacion_habilitada = False
+
+    def habilitar_variacion(self, habilitada=True):
+        """
+        Habilita o deshabilita la variación del parámetro con el tiempo. Si está deshabilitada, variar_parametro() no tendrá efecto
+        :return:
+        """
+        self.variacion_habilitada = habilitada
+
     @abstractmethod
-    def seleccionar_accion(self, agente, parametro):
+    def variar_parametro(self):
+        """
+        Varia el parámetro de la variante
+        :return:
+        """
+
+    @abstractmethod
+    def seleccionar_accion(self, agente):
         """
         Selecciona una acción
 
@@ -16,9 +36,15 @@ class Politica(ABC):
 
 
 class EpsilonGreedy(Politica):
-    def __init__(self, semilla_random=0):
-        random.seed(semilla_random)
-    def seleccionar_accion(self, agente, epsilon):
+    def __init__(self, epsilon, decaimiento_epsilon, semilla_random=0):
+        super().__init__(epsilon, decaimiento_epsilon, semilla_random)
+
+    def variar_parametro(self):
+        if self.variacion_habilitada:
+            self.parametro *= self.variacion_parametro  # Epsilon decae exponencialmente
+
+    def seleccionar_accion(self, agente):
+        epsilon = self.parametro
         if random.uniform(0, 1) < epsilon: #Tomamos una acción aleatoria con probabilidad epsilon
             accion = agente.entorno.action_space.sample()
         else: #Con probabilidad (1-epsilon) elegimos la mejor acción según la matriz Q
@@ -27,8 +53,15 @@ class EpsilonGreedy(Politica):
 
 
 class SoftMax(Politica):
-    def seleccionar_accion(self, agente, parametro_variante):
-        probabilidades = self.__softmax(agente.Q[agente.estado], sys.maxsize)
+    def __init__(self, t, incremento_t, semilla_random=0):
+        super().__init__(t, incremento_t, semilla_random)
+
+    def variar_parametro(self):
+        if self.variacion_habilitada:
+            self.parametro -= self.variacion_parametro  # T disminuye linealmente
+
+    def seleccionar_accion(self, agente):
+        probabilidades = self.__softmax(agente.Q[agente.estado], self.parametro)
         rand = random.uniform(0, 1)
         # Vamos a elegir la acción que se corresponde con esa probabilidad. Para ello cogemos la primera accion cuya probabilidad acumulada supera al número aleatorio
         i = 0
